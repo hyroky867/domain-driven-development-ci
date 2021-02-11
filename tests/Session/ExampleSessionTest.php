0d4293e6c231b361452defb7578650d2b8164efb
hyroky867 <handm755@gmail.com>
2021-02-11 20:57:28 +0900

    fix: ディレクトリを大文字に修正

---
+++ tests/Session/ExampleSessionTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Session;
+
+class ExampleSessionTest extends \Tests\Support\SessionTestCase
+{
+	public function setUp(): void
+	{
+		parent::setUp();
+	}
+
+	public function testSessionSimple()
+	{
+		$this->session->set('logged_in', 123);
+
+		$value = $this->session->get('logged_in');
+
+		$this->assertEquals(123, $value);
+	}
+}
---
+++ tests/Unit/HealthTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+class HealthTest extends \CodeIgniter\Test\CIUnitTestCase
+{
+	public function setUp(): void
+	{
+		parent::setUp();
+	}
+
+	public function testIsDefinedAppPath(): void
+	{
+		$test = defined('APPPATH');
+
+		$this->assertTrue($test);
+	}
+
+	public function testBaseUrlHasBeenSet(): void
+	{
+		$env = $config = false;
+
+		// First check in .env
+		if (is_file(HOMEPATH . '.env'))
+		{
+			$env = (bool) preg_grep("/^app\.baseURL = './", file(HOMEPATH . '.env'));
+		}
+
+		// Then check the actual config file
+		$reader = new \Tests\Support\Libraries\ConfigReader();
+		$config = ! empty($reader->baseURL);
+
+		$this->assertTrue($env || $config);
+	}
+}
