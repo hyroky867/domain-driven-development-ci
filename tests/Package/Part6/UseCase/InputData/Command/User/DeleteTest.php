348052b4e1df699489d4d3a6f3047f3cf31163b7
hyroky867 <handm755@gmail.com>
2021-02-23 18:53:24 +0900

    Delete: InputData

---
+++ tests/Package/Part6/UseCase/InputData/Command/User/DeleteTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Package\Part6\UseCase\InputData\Command\User;
+
+use Package\Part6\UseCase;
+use Tests\PHPUnitTestCase;
+
+final class DeleteTest extends PHPUnitTestCase
+{
+    /**
+     * @test
+     */
+    public function construct_値がセットできるべき(): void
+    {
+        $user_id = '2344329';
+        $actual = new UseCase\InputData\Command\User\Delete($user_id);
+
+        parent::assertSame($user_id, $actual->user_id);
+    }
+}
