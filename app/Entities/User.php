f5a28b2647f4f2e25e6feac9617692f20bcf46c6
hyroky867 <handm755@gmail.com>
2021-02-23 16:50:28 +0900

    add: email property

--- app/Entities/User.php
+++ app/Entities/User.php
@@ -10,12 +10,14 @@ use CodeIgniter\Entity;
  * @property-read int $id
  * @property-read string $user_id
  * @property-read string $name
+ * @property-read string|null $mail_address
  */
 final class User extends Entity
 {
     private ?int $id;
     private ?string $user_id;
     private ?string $name;
+    private ?string $mail_address;
 
     /**
      * @param array<string, mixed>|null $data
@@ -27,5 +29,6 @@ final class User extends Entity
         $this->id = $data['id'] ?? null;
         $this->user_id = $data['user_id'] ?? null;
         $this->name = $data['name'] ?? null;
+        $this->mail_address = $data['mail_address'] ?? null;
     }
 }
