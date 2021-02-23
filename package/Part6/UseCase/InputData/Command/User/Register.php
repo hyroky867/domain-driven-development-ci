76f561416f6c1c3d58a8b319afb1ad0d8acd4031
hyroky867 <handm755@gmail.com>
2021-02-23 18:36:15 +0900

    Register: InputData

---
+++ package/Part6/UseCase/InputData/Command/User/Register.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Package\Part6\UseCase\InputData\Command\User;
+
+use Package\ReadOnlyTrait;
+
+/**
+ * @property-read string $name
+ */
+final class Register
+{
+    use ReadOnlyTrait;
+
+    private string $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+}
